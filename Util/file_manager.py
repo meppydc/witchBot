a2f87c73c115c1b206c9bc63b9b4f4fa474dc070
meppydc <49413486+meppydc@users.noreply.github.com>
2020-08-10 01:27:31 -0400

    started file manager

---
+++ Util/file_manager.py
@@ -0,0 +1,42 @@
+from functools import reduce
+
+def file_to_array(name):
+    file = open(name,"r")
+    result = file.readlines()
+    file.close()
+    print(f"Read array {name}")
+    return result
+
+def array_to_file(name, a):
+    file = open(name,"w")
+    file.writelines(a)
+    file.close()
+    print(f"Wrote array to {name}")
+
+
+def file_to_dict(name):
+    file = open(name,"r")
+    lines = file.readlines()
+    file.close()
+    result = {}
+    for line in lines:
+        split = line.split(" ")
+        result[split[0]] = split[1:]
+        print(f"Ready dict {name}")
+    return result
+
+def dict_to_file(name, d):
+    lines = []
+    for key, value in d:
+        lines.append(f"{key}{reduce(lambda s,a: s + ' ' + str(a), value, '')}")
+    file = open(name,'w')
+    file.writelines(lines)
+    file.close
+    print(f"Wrote dict to {name}")
+
+if __name__ == "__main__":
+    #status = file_to_array('status.txt')
+    #print(status[-1])
+    #status[-1] = "Tax Fraud!"
+    #array_to_file("status.txt", status)
+    pass
