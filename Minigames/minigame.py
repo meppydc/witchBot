405f538454e42d210e3ceddc5ddb5afb18dad24a
meppydc <49413486+meppydc@users.noreply.github.com>
2020-08-25 20:01:07 -0400

    start leaderboard work

--- Minigames/minigame.py
+++ Minigames/minigame.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 from Util import util
+from Util import file_manager
 
 from threading import Timer
 
@@ -14,6 +15,8 @@ class Minigame:
         self.finished = False
         self.timer = Timer(120.0, self.timeUp)
         self.timer.start()
+        self.start = util.get_time()
+        
         
 
     def timeUp(self):
@@ -29,7 +32,7 @@ class Minigame:
         if not self.user:
             return True
         
-        if self.user == user:
+        if self.user.id == user.id:
             return True
         return False
 
@@ -47,10 +50,52 @@ class Minigame:
         if self.validate(input):
             if input == "h":
                 await self.channel.send("You won.")
-                self.finished = True
+                await self.finish()
                 return
             self.resetTimer()
             await self.output(input)
             await self.instruction()
 
+    async def finish(self):
+        self.finished = True
+        end = util.get_time()
+        delta = util.compare_time(self.start, end, 0)
+
+        lb = file_manager.file_to_array("Minigames/" + self.name.lower().replace(" ", "_") + "_leaderboard.txt", False)
+        i = 0
+        while i < 10:
+            if delta < int(lb[i + 10]):
+                lb.insert(i + 10, str(delta))
+                lb.insert(i + 1, str(self.user))
+                print(lb)
+                await self.channel.send("Updating leaderboard...")
+                lb.pop(10)
+                lb.pop(20)
+                print(lb)
+                file_manager.array_to_file("Minigames/" + self.name.lower().replace(" ", "_") + "_leaderboard.txt", lb)
+                break 
+            i += 1
+        
+
     
+
+#1
+#2
+#3
+#4
+#5
+#6
+#7
+#8
+#9
+#10
+#999
+#999
+#999
+#999
+#999
+#999
+#999
+#999
+#999
+#999
