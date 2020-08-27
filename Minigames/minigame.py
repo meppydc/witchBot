e316eaed7a8d5d85bf407dd4c51555123cd5019a
meppydc <49413486+meppydc@users.noreply.github.com>
2020-08-27 18:16:07 -0400

    basic minigame code, use jsons later

--- Minigames/minigame.py
+++ Minigames/minigame.py
@@ -49,7 +49,7 @@ class Minigame:
     async def process(self, input):
         if self.validate(input):
             if input == "h":
-                await self.channel.send("You won.")
+                
                 await self.finish()
                 return
             self.resetTimer()
@@ -60,42 +60,18 @@ class Minigame:
         self.finished = True
         end = util.get_time()
         delta = util.compare_time(self.start, end, 0)
+        await self.channel.send(f"You won in {util.secsFormat(delta)}.")
 
         lb = file_manager.file_to_array("Minigames/" + self.name.lower().replace(" ", "_") + "_leaderboard.txt", False)
         i = 0
         while i < 10:
-            if delta < int(lb[i + 10]):
+            if delta < float(lb[i + 10]):
                 lb.insert(i + 10, str(delta))
-                lb.insert(i + 1, str(self.user))
-                print(lb)
+                lb.insert(i, str(self.user))
                 await self.channel.send("Updating leaderboard...")
                 lb.pop(10)
                 lb.pop(20)
-                print(lb)
-                file_manager.array_to_file("Minigames/" + self.name.lower().replace(" ", "_") + "_leaderboard.txt", lb)
+                file_manager.array_to_file("Minigames/" + self.name.lower().replace(" ", "_") + "_leaderboard.txt", lb, True)
                 break 
             i += 1
         
-
-    
-
-#1
-#2
-#3
-#4
-#5
-#6
-#7
-#8
-#9
-#10
-#999
-#999
-#999
-#999
-#999
-#999
-#999
-#999
-#999
-#999
